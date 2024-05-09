8de0b4f54135cf0e9a118a0b8a2c43352e3de296
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-05-09 14:35:32 +0300

    Initial Commit

---
+++ SwiftBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 9.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftBootcamp/SwiftBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootcampApp.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
