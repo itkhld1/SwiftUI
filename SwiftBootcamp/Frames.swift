d0dbaca129ac0cdee6b5fa8feda6819b469d4fe1
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-05-14 11:55:15 +0300

    SwiftUI

---
+++ SwiftBootcamp/Frames.swift
@@ -0,0 +1,36 @@
+//
+//  Frames.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 10.05.2024.
+//
+
+import SwiftUI
+
+struct Frames: View {
+    var body: some View {
+        Text("SwiftUI practice!")
+            .background(Color.orange)
+            .foregroundColor(Color.white)
+            //.foregroundColor(Color.white)
+            //.frame(width: 200, height: 200, alignment: .center)
+            //.background(Color.red)
+            //.frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .center)
+            //.background(Color.primary)
+            //.frame(height: 100)
+            //.background(Color.yellow)
+            //.frame(width: 150)
+            //.background(Color.brown)
+            //.frame(maxWidth: .infinity, alignment: .leading)
+            //.background(Color.green)
+            //.frame(height: 400)
+            //.background(Color.blue)
+            //.frame(maxHeight: .infinity, alignment: .top)
+            //.background(Color.red)
+        
+    }
+}
+
+#Preview {
+    Frames()
+}
