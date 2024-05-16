6effd92ba86f8e687878bcc6dc495f09d39a63ec
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-05-16 14:29:13 +0300

    SwiftUI

---
+++ SwiftBootcamp/MyScrollView.swift
@@ -0,0 +1,38 @@
+//
+//  MyScrollView.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 11.05.2024.
+//
+
+import SwiftUI
+
+struct MyScrollView: View {
+    
+    let clrs: [Color] = [.red, .green, .blue]
+    var body: some View {
+
+        ScrollView {
+//            VStack {
+//                ForEach(0..<20) { index in
+//                    ScrollView(.horizontal, showsIndicators: true ,content: {
+//                        HStack {
+//                            ForEach(0..<20) { index in
+//                                RoundedRectangle(cornerRadius: 25.0)
+//                                    .fill(Color.white)
+//                                    .frame(width: 200, height: 200)
+//                                    .shadow(radius: 10)
+//                                    .padding()
+//                            }
+//                        }
+//                    })
+//                }
+//            }
+        }
+    }
+}
+
+#Preview {
+    MyScrollView()
+}
+
