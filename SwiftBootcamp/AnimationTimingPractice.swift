78a52ca682456efd246ed3573a1e0f550f3222cd
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-06-10 19:03:34 +0300

    SwiftUI

--- SwiftBootcamp/AnimationTimingPractice.swift
+++ SwiftBootcamp/AnimationTimingPractice.swift
@@ -34,11 +34,13 @@ struct AnimationTimingPractice: View {
                 .animation(.snappy(duration: duretion), value: isAnimated)
             
             RoundedRectangle(cornerRadius: 20)
-                .shadow(color: .blue, radius: 10)
                 .frame(width: isAnimated ? 350 : 50, height: 100)
                 .animation(.spring(response: 3.5,
-                                   dampingFraction: 0.5, blendDuration: 1.0 ), value: isAnimated)
+                    dampingFraction: 0.5,
+                    blendDuration: 1.0 ),
+                    value: isAnimated)
         }
+        .shadow(color: .gray, radius: 10, x: -7)
     }
 }
 
