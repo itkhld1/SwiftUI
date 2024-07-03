0a00707565c573d2ff0e27e7408132843acecb8f
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-07-03 15:43:23 +0300

    SwiftUI

---
+++ SwiftBootcamp/AnylayoutPractice.swift
@@ -0,0 +1,18 @@
+//
+//  AnylayoutPractice.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 2024-06-30.
+//
+
+import SwiftUI
+
+struct AnylayoutPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnylayoutPractice()
+}
