b2324e204ddead58397616387b5c57aa9f96f478
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-12-03 14:26:13 +0300

    SwiftUI

---
+++ SwiftBootcamp/MacroPractice.swift
@@ -0,0 +1,21 @@
+//
+//  MacroPractice.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 2024-07-03.
+//
+
+// ObserviableObjects
+// StateObjects
+// EnviromentObjects
+import SwiftUI
+
+struct MacroPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MacroPractice()
+}
