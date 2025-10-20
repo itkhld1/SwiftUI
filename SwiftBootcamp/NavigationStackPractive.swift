41d63374aa1c88c6fffa2024ef8f182fc93aea8c
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2025-10-20 14:20:36 +0300

    SwiftUI

---
+++ SwiftBootcamp/NavigationStackPractive.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackPractive.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 2024-06-28.
+//
+
+import SwiftUI
+
+struct NavigationStackPractive: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackPractive()
+}
