6b422b57b541f63d8f8f78ddd74694326ef0b1a4
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2026-01-08 20:42:55 +0300

    SwiftUI

--- SwiftBootcamp/SwiftBootcampApp.swift
+++ SwiftBootcamp/SwiftBootcampApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftBootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            //MagnificationGesturePractice()
+            //ActionSheetPractice()
+            RotationGesturePractice()
         }
     }
 }
