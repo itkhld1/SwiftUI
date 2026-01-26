59c643d682c5debc79b62f692615f401d7e4a89e
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2026-01-26 14:51:05 +0300

    SwiftUI

---
+++ SwiftBootcamp/TextSelectionPractice.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionPractice.swift
+//  SwiftBootcamp
+//
+//  Created by itkhld on 2024-06-28.
+//
+
+import SwiftUI
+
+struct TextSelectionPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionPractice()
+}
